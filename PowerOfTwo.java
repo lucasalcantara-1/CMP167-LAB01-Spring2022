135918fb176dd9e8efcc00934e21bb92ac2308b0
lucasalcantara-1 <100226872+lucasalcantara-1@users.noreply.github.com>
2022-03-08 12:38:12 -0500

    Add files via upload

---
+++ PowerOfTwo.java
@@ -0,0 +1,19 @@
+/** 
+ * 
+ * @author Lucas Alcantara
+ * 
+ */
+
+public class PowerOfTwo 
+{
+	public static void main(String[] args) 
+	{
+	int numPowerOfTwo = 0; 
+	for (int i = 1; i <= 5; i+= 1)
+	{
+		numPowerOfTwo = numPowerOfTwo + i; 
+	}
+	
+	System.out.println(numPowerOfTwo);
+}
+	}
